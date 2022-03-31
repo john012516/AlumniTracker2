712c78b13edbd54ce2b1feda5a3eba9cb9cfe44f
momoo24 <monettesoriano9@gmail.com>
2022-03-31 22:35:21 +0800

    pop up message updateprofile/employed/login

--- CITAT/views.py
+++ CITAT/views.py
@@ -1016,6 +1016,8 @@ def accountSettings(request):
 		form = AlumniForm(request.POST, request.FILES, instance=alumni)
 		if form.is_valid():
 			form.save()
+		messages.success(request, 'Successfully Submitted')
+		return redirect('account')
 
 	context = {'form':form}
 	return render(request, 'CITAT/account_settings.html', context)
@@ -1032,6 +1034,7 @@ def employed(request):
 			instance = form.save(commit = False)
 			instance.alumni = request.user.alumni
 			instance.save()
+			messages.success(request, 'Successfully Submitted')
 			return redirect('account')
 
 			
